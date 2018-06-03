523f872b4b751f7d336acd7e968d7cfec0b5bb3b
Pankaj Doharey <pankajdoharey@gmail.com>
2018-06-03 17:46:45 +0530

    * The commit Updates deps.edn to use figwheel-main * Updates deps.edn to use rebel-readline * Adds loading of hiccup templates from edn files through xhr request. * Adds CSS changes to main.cssin Profile pic style.

---
+++ src/resume/core.cljs
@@ -0,0 +1,46 @@
+(ns resume.core
+  (:require-macros [hiccups.core :as hiccups :refer [html]])
+  (:require [cljs.reader :as rdr :refer [read-string]]
+            [hiccups.runtime :as hiccupsrt]
+            [clojure.browser.dom :as bdom]
+            [goog.events :as gev]
+            [goog.dom :as dom]))
+
+(defn hiccup->html [hiccup]
+  (apply str (mapv #(html %) hiccup)))
+
+(defn html->node [tmpl]
+  (dom/constHtmlToNode
+   (.from goog.string/Const (hiccup->html tmpl))))
+
+(defn xhr->html [req]
+  (html->node (read-string (.getResponseText req))))
+
+(defn append-child [id child]
+  (let [parent (name id)]
+    (cond
+      (= id :head) (bdom/append (dom/getElementByTagNameAndClass parent) child)
+      :else (bdom/append (dom/getElement parent) child))))
+
+(defn slurp [path callback]
+  (let [xhr (goog.net.XhrIo.) complt goog.net.EventType.COMPLETE]
+   (gev/listen xhr complt #(callback (xhr->html xhr)))
+   (.send xhr path)))
+
+(defn style-sheet [callback]
+  (slurp "/hiccup/style-sheet.hic.edn" callback))
+
+(defn header-tmpl [callback]
+  (slurp "/hiccup/header-tmpl.hic.edn" callback))
+
+(defn console-welcome []
+  (.clear js/console)
+  (.log js/console "Hey Welcome to metacritical's cv."))
+
+(defn main []
+  (bdom/remove-children :app)
+  (console-welcome)
+  (style-sheet #(append-child :head %))
+  (header-tmpl #(append-child :app %)))
+
+(.addEventListener js/window "DOMContentLoaded" (main))
