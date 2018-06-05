7d16f6b268010f7a3ea6f80d2e8fc7ebda37ea4b
Pankaj Doharey <pankajdoharey@gmail.com>
2018-06-05 19:52:59 +0530

    Moves html related functions in its own file

--- src/resume/core.cljs
+++ src/resume/core.cljs
@@ -1,31 +1,6 @@
 (ns resume.core
-  (:require-macros [hiccups.core :as hiccups :refer [html]])
-  (:require [cljs.reader :as rdr :refer [read-string]]
-            [hiccups.runtime :as hiccupsrt]
-            [clojure.browser.dom :as bdom]
-            [goog.events :as ev]
-            [goog.dom :as dom]))
-
-(defn hiccup->html [hiccup]
-  (apply str (mapv #(html %) hiccup)))
-
-(defn html->node [tmpl]
-  (dom/constHtmlToNode
-   (.from goog.string/Const (hiccup->html tmpl))))
-
-(defn xhr->html [req]
-  (html->node (read-string (.getResponseText req))))
-
-(defn append-child [id child]
-  (let [parent (name id)]
-    (case id
-      :head (bdom/append (dom/getElementByTagNameAndClass parent) child)
-      (bdom/append (dom/getElement parent) child))))
-
-(defn slurp [path callback]
-  (let [xhr (goog.net.XhrIo.) complt goog.net.EventType.COMPLETE]
-   (ev/listen xhr complt #(callback (xhr->html xhr)))
-   (.send xhr path)))
+  (:require [clojure.browser.dom :as bdom]
+            [resume.html-utils :as utils]))
 
 (defn console-welcome []
   (.clear js/console)
@@ -34,7 +9,7 @@
 (defn main []
   (bdom/remove-children :app)
   (console-welcome)
-  (slurp "/hiccup/style-sheet.hic.edn" #(append-child :head %))
-  (slurp "/hiccup/header-tmpl.hic.edn" #(append-child :app %)))
+  (utils/slurp "/hiccup/style-sheet.hic.edn" #(utils/append-child :head %))
+  (utils/slurp "/hiccup/header-tmpl.hic.edn" #(utils/append-child :app %)))
 
 (.addEventListener js/window "DOMContentLoaded" (main))
